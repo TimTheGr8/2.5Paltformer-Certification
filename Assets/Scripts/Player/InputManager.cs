72ef676771fe5ae062ec1692907ec4fb4a202971
TimTheGr8 <tjanssen4@gmail.com>
2023-08-29 21:18:59 -0500

    Added the movement and jump functionality.

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private Player _player;
+
+    private PlayerInputs _inputs;
+
+    private void Awake()
+    {
+        if (_player == null)
+            Debug.LogError("There is no player for the Input Manager.");
+
+        _inputs = new PlayerInputs();
+        InitializeInputs();
+    }
+
+    private void InitializeInputs()
+    {
+        _inputs.Player.Enable();
+        _inputs.Player.Move.performed += Move_performed;
+        _inputs.Player.Move.canceled += Move_canceled;
+        _inputs.Player.Jump.performed += Jump_performed;
+    }
+
+    private void Jump_performed(InputAction.CallbackContext context)
+    {
+        _player.Jump();
+    }
+
+    private void Move_canceled(InputAction.CallbackContext context)
+    {
+        _player.SetWalk(0);
+    }
+
+    private void Move_performed(InputAction.CallbackContext context)
+    {
+        _player.SetWalk(context.ReadValue<float>());
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private int _lives = 3;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.3f;
+    [SerializeField]
+    private float _jumpHeight = 20.0f;
+
+    private CharacterController _controller;
+    private float _walk = 0.0f;
+    private Vector3 _direction;
+    private Vector3 _velocity;
+    private float _yVelocity = 0.0f;
+    private int _jumpCount = 0;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        if (_controller == null)
+            Debug.LogError("There is no Character Controller on the Player.");
+    }
+
+    void FixedUpdate()
+    {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        if (!_controller.isGrounded)
+            _yVelocity -= _gravity;
+
+        if(_controller.isGrounded)
+        {
+            _direction = new Vector3(0, 0, _walk);
+            _jumpCount = 0;
+            _velocity = _direction * _speed;
+        }
+        _velocity.y = _yVelocity;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if (_jumpCount < 1)
+        {
+            _jumpCount++;
+            if (_jumpCount == 1)
+                _yVelocity = _jumpHeight;
+            if (_jumpCount == 2)
+                _yVelocity += _jumpHeight;
+        }
+    }
+
+    public void SetWalk(float walk)
+    {
+        _walk = walk;
+    }
+}
